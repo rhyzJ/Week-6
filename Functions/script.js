a428e4aca9fd71c91792c15ef2b81bbc48b0deed
Rhya Johns <rhyannonjohns@gmail.com>
2024-05-08 18:30:25 +1200

    function calls & parameter examples

--- Functions/script.js
+++ Functions/script.js
@@ -1,1 +1,21 @@
 console.log("Functions!");
+
+//----- BASIC FUNCTION CALLS-----//
+// to call a function we simply specify its name and open/close brackets()
+
+function myFirstFunction() {
+  console.log("I just called my first function");
+}
+
+myFirstFunction();
+myFirstFunction();
+
+//-----FUNCTION PARAMETERS-----//
+// optional
+// go inside the smooth brackets
+
+function sum(num1, num2){
+    console.log(num1+num2)
+}
+
+sum(3,4);
